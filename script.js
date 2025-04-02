d061b32715979b89c28cba138eb0ac15dd8e80c8
Aakarshan Jorge <aaka022009@gmail.com>
2025-04-02 22:55:04 +0200

    Completed the JS and added a border effect when there is no email, password or name inputted

---
+++ script.js
@@ -0,0 +1,81 @@
+let emailBox = document.querySelector(".email-box");
+let nameBox = document.querySelector(".name-box");
+let passwordBox = document.querySelector(".password-box");
+let signUpButton = document.querySelector("button");
+
+function changeBorderColor(element) {
+    element.classList.add("error");
+}
+
+function inputEmail() {
+    if (emailBox.value === "") {
+        changeBorderColor(emailBox);
+        return "Email field is empty";
+    }
+    return false;
+}
+
+function inputPassword () {
+    if(passwordBox.value === "") {
+        changeBorderColor(passwordBox);
+
+        return "Password field is empty";
+    }
+    return false;
+}
+
+function inputName () {
+    if(nameBox.value === ""){
+        changeBorderColor(nameBox);
+        return "Name field is empty";
+    }
+    return false;
+}
+
+function passwordLength(){
+    if(passwordBox.value.length < 8) {
+        changeBorderColor(passwordBox)
+        return "Password is shorter than 8 characters";
+    }
+    return false;
+} 
+
+function raiseAlerts() {
+    let alerts = [];
+    let temp = inputEmail();
+
+    if(temp != false) {
+        alerts.push(temp);
+    }
+    temp = inputPassword();
+
+    if(temp != false) {
+        alerts.push(temp);
+    }
+    temp = inputName();
+
+    if(temp != false) {
+        alerts.push(temp);
+    }
+    temp = passwordLength();
+
+    if (temp != false) {
+        alerts.push(temp);
+    }
+
+    return alerts;
+}
+
+function alertPrint(alerts) {
+    for (let i = 0; i < alerts.length; i++) {
+        alert(alerts[i]);
+    }
+}
+
+signUpButton.addEventListener('click', () => {
+    raiseAlerts();
+    alertPrint(raiseAlerts());
+})
+
+
+
